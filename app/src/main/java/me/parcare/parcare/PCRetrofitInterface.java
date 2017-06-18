604968de4a50e349e27a79b49e4fbd71b94557c3
Zula Li <zulali.32@gmail.com>
2017-06-18 09:53:29 -0700

    Added Retrofit Interface class (contents set as a placeholder until we get server functionality)

---
+++ app/src/main/java/me/parcare/parcare/PCRetrofitInterface.java
@@ -0,0 +1,15 @@
+package me.parcare.parcare;
+
+import retrofit2.Call;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+/**
+ * Created by Zula on 6/18/17.
+ */
+
+public interface PCRetrofitInterface {
+    // Temp placeholders in place until server functionality is actually set up.
+    @POST("URL Endpoint Here")
+    Call<LatLngPC> getClosestSpot(@Query("Variable name 1 Here") String var1, @Query("Variable name 2 Here") String var2);
+}
