5d51300a69b639eee175e1f6df83822d8a455d80
DarkTerbear <terranceli.terbear@gmail.com>
2017-06-25 10:10:45 -0700

    Completed UI and local storage of profile
    Need to implement autocomplete for addresses

--- app/src/main/java/me/parcare/parcare/ProfileDialogFragment.java
+++ app/src/main/java/me/parcare/parcare/ProfileDialogFragment.java
@@ -3,14 +3,16 @@ package me.parcare.parcare;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -20,7 +22,13 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+
+import static com.mapbox.mapboxsdk.Mapbox.getApplicationContext;
 
 /**
  * Created by Terrance on 6/24/2017.
@@ -29,15 +37,15 @@ import java.io.IOException;
 public class ProfileDialogFragment extends DialogFragment {
 
     SharedPreferences sharedPreferences;
-    boolean editImage = false;
     ImageView profilePictureImageView;
     EditText nameEditText, homeAddressEditText, workAddressEditText;
     TextView enterNameTextView;
-    FloatingActionButton editFAB;
 
     private static final String SP_USER_NAME_TAG = "user_name";
     private static final String SP_USER_HOME_ADDRESS_TAG = "user_home_address";
     private static final String SP_USER_WORK_ADDRESS_TAG = "user_work_address";
+    private static final String USER_PROFILE_PICTURE_FILENAME_TAG = "user_profile_picture.png";
+    private static final String USER_PROFILE_PICTURE_DIRECTORY_TAG = "profile_picture_directory_path";
     private static final int PICK_IMAGE_REQUEST_CALLBACK = 1;
 
 
@@ -57,55 +65,39 @@ public class ProfileDialogFragment extends DialogFragment {
         homeAddressEditText = (EditText) dialogView.findViewById(R.id.home_address_edittext);
         workAddressEditText = (EditText) dialogView.findViewById(R.id.work_address_edittext);
         enterNameTextView = (TextView) dialogView.findViewById(R.id.enter_name_label);
-        editFAB = (FloatingActionButton) dialogView.findViewById(R.id.edit_fab);
 
         nameEditText.setText(sharedPreferences.getString(SP_USER_NAME_TAG, "Your Name"));
         homeAddressEditText.setText(sharedPreferences.getString(SP_USER_HOME_ADDRESS_TAG, ""));
         workAddressEditText.setText(sharedPreferences.getString(SP_USER_WORK_ADDRESS_TAG, ""));
-        //TODO Retrieve image from internal storage
+
+        String directoryPath = sharedPreferences.getString(USER_PROFILE_PICTURE_DIRECTORY_TAG, "no_picture");
+        if (directoryPath.equals("no_picture")) {
+            //TODO Set imageview to preset profile image
+        } else {
+            profilePictureImageView.setImageBitmap(openImage(directoryPath));
+        }
+
 
         profilePictureImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (editImage) {
-                    Intent chooseImageIntent = new Intent();
-                    chooseImageIntent.setType("image/*");
-                    chooseImageIntent.setAction(Intent.ACTION_GET_CONTENT);
-                    startActivityForResult(Intent.createChooser(chooseImageIntent, "Select a picture"), PICK_IMAGE_REQUEST_CALLBACK);
-                }
-            }
-        });
-
-        editFAB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                nameEditText.setEnabled(true);
-                homeAddressEditText.setEnabled(true);
-                workAddressEditText.setEnabled(true);
-                editImage = true;
+                Intent chooseImageIntent = new Intent();
+                chooseImageIntent.setType("image/*");
+                chooseImageIntent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(chooseImageIntent, "Select a picture"), PICK_IMAGE_REQUEST_CALLBACK);
             }
         });
 
         builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                nameEditText.setEnabled(false);
-                homeAddressEditText.setEnabled(false);
-                workAddressEditText.setEnabled(false);
-
-                if (nameEditText.getText().toString().equals("")) {
-                    enterNameTextView.setVisibility(View.VISIBLE);
-                } else {
-                    SharedPreferences.Editor editor = sharedPreferences.edit();
-                    editor.putString(SP_USER_NAME_TAG, nameEditText.getText().toString());
-                    editor.putString(SP_USER_HOME_ADDRESS_TAG, homeAddressEditText.getText().toString());
-                    editor.putString(SP_USER_WORK_ADDRESS_TAG, workAddressEditText.getText().toString());
-                    editor.commit();
-
-                    //TODO POST request to server, update profile and image
-                }
+                /* KEEP THIS METHOD
+                    It is only used to show the Save button, the actual behavior of the save button
+                    is defined below in onResume()
+                 */
             }
         });
+
 
         builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
             @Override
@@ -128,7 +120,22 @@ public class ProfileDialogFragment extends DialogFragment {
                     Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), imageURI);
                     Log.d("IMAGEPICKER", String.valueOf(bitmap));
 
-                    profilePictureImageView.setImageBitmap(bitmap);
+
+                    //Check if image is ridiculously large (over 512 on largest dimension)
+                    if (Math.max(bitmap.getHeight(), bitmap.getWidth()) > 512) {
+                        //Scale the bitmap
+                        int scaledHeight = 0, scaledWidth = 0;
+                        double scaleFactor = Math.max(bitmap.getHeight(), bitmap.getWidth()) / (float) 512;
+
+                        scaledHeight = (int) Math.floor(bitmap.getHeight() / scaleFactor);
+                        scaledWidth = (int) Math.floor(bitmap.getWidth() / scaleFactor);
+
+                        Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, scaledWidth, scaledHeight, false);
+
+                        profilePictureImageView.setImageBitmap(scaledBitmap);
+                    } else {
+                        profilePictureImageView.setImageBitmap(bitmap);
+                    }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -149,25 +156,58 @@ public class ProfileDialogFragment extends DialogFragment {
                 @Override
                 public void onClick(View v)
                 {
-                    nameEditText.setEnabled(false);
-                    homeAddressEditText.setEnabled(false);
-                    workAddressEditText.setEnabled(false);
-
                     if (nameEditText.getText().toString().equals("")) {
                         enterNameTextView.setVisibility(View.VISIBLE);
                     } else {
-                        SharedPreferences.Editor editor = sharedPreferences.edit();
-                        editor.putString("user_name", nameEditText.getText().toString());
-                        editor.putString("user_home_address", homeAddressEditText.getText().toString());
-                        editor.putString("user_work_address", workAddressEditText.getText().toString());
-                        editor.apply();
+                        String directory = saveImage(((BitmapDrawable) profilePictureImageView.getDrawable()).getBitmap());
 
-                        //TODO POST request to server, update profile and image
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString(SP_USER_NAME_TAG, nameEditText.getText().toString());
+                        editor.putString(SP_USER_HOME_ADDRESS_TAG, homeAddressEditText.getText().toString());
+                        editor.putString(SP_USER_WORK_ADDRESS_TAG, workAddressEditText.getText().toString());
+                        editor.putString(USER_PROFILE_PICTURE_DIRECTORY_TAG, directory);
+                        editor.commit();
 
                         d.dismiss();
+                        //TODO POST request to server, update profile and image
                     }
                 }
             });
         }
     }
+
+    private String saveImage(Bitmap bitmapImage){
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        // path to /data/data/yourapp/app_data/imageDir
+        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+        // Create imageDir
+        File mypath = new File(directory,USER_PROFILE_PICTURE_FILENAME_TAG);
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(mypath);
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return directory.getAbsolutePath();
+    }
+
+    private Bitmap openImage(String path) {
+        try {
+            File f = new File(path, USER_PROFILE_PICTURE_FILENAME_TAG);
+            return BitmapFactory.decodeStream(new FileInputStream(f));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
