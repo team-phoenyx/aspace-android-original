be389343fe7a50f548eb7990415480eb5ceef957
DarkTerbear <terranceli.terbear@gmail.com>
2017-07-16 20:30:17 -0700

    Completed text to speech logic

--- app/src/main/java/com/aspace/aspace/MainActivity.java
+++ app/src/main/java/com/aspace/aspace/MainActivity.java
@@ -123,6 +123,7 @@ public class MainActivity extends AppCompatActivity implements PermissionsListen
     private MapboxNavigation navigation;
     private LatLng clickedSpotLatLng;
     private com.mapbox.services.api.directions.v5.models.DirectionsRoute route;
+    private LegStep lastUpcomingStep;
     private TextToSpeech textToSpeech;
     private Position navDestination;
     private Polyline drivingRoutePolyline;
@@ -262,6 +263,7 @@ public class MainActivity extends AppCompatActivity implements PermissionsListen
                         break;
                     case DEPART_ALERT_LEVEL:
                         Toast.makeText(MainActivity.this, "DEPART", Toast.LENGTH_LONG).show();
+                        textToSpeech.speak(routeProgress.getCurrentLeg().getSteps().get(0).getManeuver().getInstruction(), TextToSpeech.QUEUE_ADD, null, null);
                         break;
                 }
             }
@@ -275,6 +277,18 @@ public class MainActivity extends AppCompatActivity implements PermissionsListen
                 RouteStepProgress routeStepProgress = routeProgress.getCurrentLegProgress().getCurrentStepProgress();
                 LegStep nextStep = routeProgress.getCurrentLegProgress().getUpComingStep();
 
+                //NEW MANEUVER
+                if (lastUpcomingStep == null || !nextStep.getManeuver().getInstruction().equals(lastUpcomingStep.getManeuver().getInstruction())) {
+                    double distance = routeStepProgress.getDistanceRemaining();
+
+                    if (distance <= 30.5 || (distance > 152.4 && distance < 182.9) || (distance > 804.7 && distance < 965.6) || (distance > 1609.3 && distance < 1649.8)) {
+
+                    } else {
+                        String distanceString = translateDistance(distance);
+                        textToSpeech.speak("In " + distanceString.replace("mi", "miles").replace("ft", "feet") + ", " + nextStep.getManeuver().getInstruction(), TextToSpeech.QUEUE_ADD, null, null);
+                    }
+                }
+
                 String maneuverType = nextStep.getManeuver().getType();
                 if (maneuverType.equalsIgnoreCase("continue")) {
                     maneuverType += "e";
@@ -282,11 +296,19 @@ public class MainActivity extends AppCompatActivity implements PermissionsListen
                 String maneuverModifier = "" + nextStep.getManeuver().getModifier();
 
                 // Updating main navigation tool bar
-                if (routeStepProgress.getDistanceRemaining() < 30.5) {
+                if (routeStepProgress.getDistanceRemaining() <= 30.5) {
                     String directionString = nextStep.getManeuver().getType() + " " + nextStep.getManeuver().getModifier();
                     navManeuverDistanceLabel.setText(Character.toUpperCase(directionString.charAt(0)) + directionString.substring(1));
+                    textToSpeech.speak(nextStep.getManeuver().getInstruction(), TextToSpeech.QUEUE_ADD, null, null);
                 }
-                else navManeuverDistanceLabel.setText("In " + translateDistance(routeStepProgress.getDistanceRemaining()));
+                else {
+                    String distanceString = translateDistance(routeStepProgress.getDistanceRemaining());
+                    navManeuverDistanceLabel.setText("In " + distanceString);
+
+                    if (distanceString.equals("1.0 mi") || distanceString.equals("1 mi")) textToSpeech.speak("In one mile, " + nextStep.getManeuver().getInstruction(), TextToSpeech.QUEUE_ADD, null, null);
+                    if (distanceString.equals("0.5 mi")) textToSpeech.speak("In half a mile, " + nextStep.getManeuver().getInstruction(), TextToSpeech.QUEUE_ADD, null, null);
+                    if (distanceString.equals("500 ft")) textToSpeech.speak("In 500 feet, " + nextStep.getManeuver().getInstruction(), TextToSpeech.QUEUE_ADD, null, null);
+                }
 
                 if (nextStep.getName().isEmpty()) navManeuverTargetLabel.setText(nextStep.getManeuver().getInstruction());
                 else navManeuverTargetLabel.setText(nextStep.getName());
@@ -308,6 +330,8 @@ public class MainActivity extends AppCompatActivity implements PermissionsListen
                 for (LegStep step : steps) {
                     Log.i(TAG + "Directions", "LEGSTEP: " + step.getName() + ", Maneuver: " + step.getManeuver().getInstruction() + ", Step distance: " + step.getDistance() + " Type: "+ step.getManeuver().getType() + " Modifier: " + step.getManeuver().getModifier());
                 }
+
+                lastUpcomingStep = nextStep;
             }
         });
 
