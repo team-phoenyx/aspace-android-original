ad7c714d92ad231bcc08b320891d0283fbe6fa48
Zula Li <zulali.32@gmail.com>
2017-07-23 14:33:08 -0700

    Added edit name button functionality (doesn't update server/save yet)

--- app/src/main/java/com/aspace/aspace/SettingsActivity.java
+++ app/src/main/java/com/aspace/aspace/SettingsActivity.java
@@ -13,16 +13,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private ImageButton toolbarExitButton;
+    private TextView myNameLabel;
     private EditText nameEditText;
     private ImageButton nameEditButton;
     private ListView myVehiclesList;
     private Button addVehicleButton;
     private Button deleteAccountButton;
-    private View dividerDummyView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +37,6 @@ public class SettingsActivity extends AppCompatActivity {
         myVehiclesList = (ListView) findViewById(R.id.settings_my_vehicle_list);
         addVehicleButton = (Button) findViewById(R.id.settings_add_vehicle_button);
         deleteAccountButton = (Button) findViewById(R.id.settings_delete_account_button);
-        dividerDummyView = findViewById(R.id.settings_my_vehicles_divider);
-
         toolbar.setTitleTextColor(Color.WHITE);
         toolbar.setTitle("Settings");
 
@@ -51,7 +50,8 @@ public class SettingsActivity extends AppCompatActivity {
         nameEditButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!nameEditText.isFocused()) {
+                Log.i("SETTINGS", "CLICKED");
+                if (!nameEditText.isFocusableInTouchMode()) {
                     nameEditText.setCursorVisible(true);
                     nameEditText.setFocusableInTouchMode(true);
                     nameEditText.setInputType(InputType.TYPE_CLASS_TEXT);
@@ -59,22 +59,11 @@ public class SettingsActivity extends AppCompatActivity {
                     InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                     imm.showSoftInput(nameEditText, InputMethodManager.SHOW_IMPLICIT);
                 } else {
+                    nameEditText.setFocusableInTouchMode(false);
                     nameEditText.clearFocus();
-                    nameEditText.setEnabled(false);
                     nameEditText.setInputType(InputType.TYPE_NULL);
                     InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                     imm.hideSoftInputFromWindow(nameEditText.getWindowToken(), 0);
-                }
-            }
-        });
-
-        nameEditButton.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    Log.i("SETTINGS", "hasFocus = true");
-                } else {
-                    Log.i("SETTINGS", "hasFocus = false");
                 }
             }
         });
